bf5662851645beaf9cebd23aab377df08eb96fb4
RedTriangle2 <alexanderrt1230@gmail.com>
2025-03-07 14:49:40 -0500

    e

---
+++ SQLiteUnity3dTemplate2025/Assets/Scripts/SimpleDB.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mono.Data.Sqlite; //calls the library of SQLite
+//https://learn.microsoft.com/en-us/dotnet/standard/data/sqlite/?tabs=net-cli
+using UnityEngine;
+
+public class SimpleDB : MonoBehaviour
+{
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
