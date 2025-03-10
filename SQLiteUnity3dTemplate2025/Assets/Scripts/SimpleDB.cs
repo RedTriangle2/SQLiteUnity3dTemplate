fe6a37b4703a0154f94e62c25ff28ab387c5d2ad
RedTriangle2 <alexanderrt1230@gmail.com>
2025-03-10 14:51:38 -0400

    create the database

--- SQLiteUnity3dTemplate2025/Assets/Scripts/SimpleDB.cs
+++ SQLiteUnity3dTemplate2025/Assets/Scripts/SimpleDB.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using Mono.Data.Sqlite; //calls the library of SQLite
 //https://learn.microsoft.com/en-us/dotnet/standard/data/sqlite/?tabs=net-cli
 using UnityEngine;
+using System.Data;
 
 public class SimpleDB : MonoBehaviour
 {
 
-    
+    private string dbName = "URI = file:Inventory.db";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //calls Method to create Database
+        CreateDB();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //TODO: CREATE METHOD FOR CreateDB()
+    private void CreateDB()
+    {
+      using(var connection = new SqliteConnection(dbName))
+      {
+        connection.Open ();
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = "CREATE TABLE IF NOT EXISTS wepons(name VARCHAR (20), qte INT)";
+            command.ExecuteNonQuery();
+        }
+        connection.Close ();
+      }
     }
 }
