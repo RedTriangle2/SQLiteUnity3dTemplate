089e65db963b230ecba2f5874df418d28d834a57
RedTriangle2 <alexanderrt1230@gmail.com>
2025-03-13 14:50:58 -0400

    h

--- SQLiteUnity3dTemplate2025/Assets/Scripts/SimpleDB.cs
+++ SQLiteUnity3dTemplate2025/Assets/Scripts/SimpleDB.cs
@@ -16,8 +16,11 @@ public class SimpleDB : MonoBehaviour
     {
         //calls Method to create Database
         CreateDB();
-        AddWeapon();
-        // AddWeapon
+        AddWeapon("pistol", 25);
+        AddWeapon("sword", 6);
+        // TODO: Call the method and delte Rifle
+        DeleteWeaspon("Rifle");
+       
         Debug.Log(" SQL started Working!");
     }
 
@@ -43,7 +46,7 @@ public class SimpleDB : MonoBehaviour
     }
 
     //TODO: create method for adding weapon into the database
-    public void AddWeapon()
+    public void AddWeapon(string weaponName, int weaponQte)
     {
       using(var connection = new SqliteConnection(dbName))
       {
@@ -51,10 +54,32 @@ public class SimpleDB : MonoBehaviour
         using (var command = connection.CreateCommand())
         {
             //SQL code goes here
-            command.CommandText = "INSERT INTO weapons (name, qte) VALUES ('Rifle', '17');";// Adds the SQL code as the argument to add a value of Rifle of 17 as the  values
+            command.CommandText = "INSERT INTO weapons (name, qte) VALUES (@name, @qte);";// Adds the SQL code as the argument to add a value of Rifle of 17 as the  values
+            //command.Parameters allows you to add and Assign the parameter with the value form within Values
+            command.Parameters.AddWithValue ("@name", weaponName);
+            command.Parameters.AddWithValue ("@qte", weaponQte);
             command.ExecuteNonQuery();
         }
         connection.Close ();
       }
     }
+
+    public void DeleteWeaspon(string weaponName)
+    {
+      using(var connection = new SqliteConnection(dbName))
+      {
+        connection.Open ();
+        using (var command = connection.CreateCommand())
+        {
+            //SQL code goes here
+            //TODO: DELETE FROM weapons WHERE name = @name
+            command.CommandText = "DELETE FROM weapons WHERE name = @name;";// Adds the SQL code as the argument to add a value of Rifle of 17 as the  values
+            //command.Parameters allows you to add and Assign the parameter with the value form within Values
+            command.Parameters.AddWithValue ("@name", weaponName);
+            command.ExecuteNonQuery();
+        }
+        connection.Close ();
+      }
+    }
+
 }
